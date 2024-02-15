2e2f0020a3f1d697404c254107774364ed24ccca
predeep kumar singroul <prasingroul4899@gmail.com>
2024-02-15 23:31:10 +0530

    User services

--- Role-Based-Login-System/src/main/java/com/login/services/UserServices.java
+++ Role-Based-Login-System/src/main/java/com/login/services/UserServices.java
@@ -1,5 +1,18 @@
 package com.login.services;
 
-public interface UserServices {
+import java.util.List;
 
+import com.login.Entity.Role;
+import com.login.Entity.User;
+import com.login.exceptions.UserException;
+
+public interface UserServices {
+	
+	User createUser(String name, String password, List<Role> roles) throws UserException;
+	List<User> getAllUsers()throws UserException;
+	User getUserByName(String name) throws UserException;
+	User getUserById(Long id) throws UserException;
+	User deleteUserById(Long id) throws UserException;
+	User updateUser(User user) throws UserException;
+	
 }
