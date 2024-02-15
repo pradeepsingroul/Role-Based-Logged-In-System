7bf61232876b2f7097dd629c7d53cf3eb0326a2b
predeep kumar singroul <prasingroul4899@gmail.com>
2024-02-16 00:13:21 +0530

    Repositories

--- Role-Based-Login-System/src/main/java/com/login/repository/UserRepository.java
+++ Role-Based-Login-System/src/main/java/com/login/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.login.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.login.Entity.User;
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long>{
 
+	Optional<User> findByEmail(String email);
 }
