b1ff7c1aa09f16be4537fb17e7e2c6753183fb04
predeep kumar singroul <prasingroul4899@gmail.com>
2024-02-16 07:09:20 +0530

    Role service implementation

--- Role-Based-Login-System/src/main/java/com/login/services/RoleServiceImpl.java
+++ Role-Based-Login-System/src/main/java/com/login/services/RoleServiceImpl.java
@@ -1,50 +1,75 @@
 package com.login.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.login.Entity.Role;
 import com.login.Entity.RoleType;
 import com.login.exceptions.RoleException;
+import com.login.exceptions.UserException;
+import com.login.repository.RoleRepository;
 
 @Service
 public class RoleServiceImpl implements RoleServices{
+	
+	@Autowired
+	private RoleRepository rRepo;
 
 	@Override
 	public Role createRole(Role role) throws RoleException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Role> opt  = rRepo.findByName(role.getName());
+		if(opt.isEmpty()) return rRepo.save(role);
+		else throw new RoleException("The role is Already exist....");
 	}
 
 	@Override
 	public List<Role> getAllRoles() throws RoleException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Role> roles = rRepo.findAll();
+		if(roles.isEmpty()) throw new RoleException("Role does not exist...");
+		else return roles;
 	}
 
 	@Override
 	public Role getRoleByName(RoleType name) throws RoleException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Role> opt  = rRepo.findByName(name);
+		if(opt.isEmpty()) throw new RoleException("The role is does not exist....");
+		else  {
+			Role role = opt.get();
+			return role;
+		}
 	}
 
 	@Override
 	public Role getRoleById(Long id) throws RoleException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Role> opt  = rRepo.findById(id);
+		if(opt.isEmpty()) throw new RoleException("The role is does not exist....");
+		else  {
+			Role role = opt.get();
+			return role;
+		}
 	}
 
 	@Override
 	public Role deleteRoleById(Long id) throws RoleException {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Role> opt  = rRepo.findById(id);
+		if(opt.isEmpty()) throw new RoleException("The role is does not exist....");
+		else  {
+			Role role = opt.get();
+			rRepo.deleteById(id);
+			return role;
+		}
 	}
 
 	@Override
-	public Role updateRole(Role Role) throws RoleException {
-		// TODO Auto-generated method stub
-		return null;
+	public Role updateRole(Role role) throws RoleException {
+		Optional<Role> opt = rRepo.findByName(role.getName());
+		if(opt.isEmpty()) throw new UserException("The role does not exist....");
+		else {
+			return rRepo.save(role);
+		}
 	}
 
 }
--- Role-Based-Login-System/src/main/java/com/login/services/UserServices.java
+++ Role-Based-Login-System/src/main/java/com/login/services/UserServices.java
@@ -2,17 +2,22 @@ package com.login.services;
 
 import java.util.List;
 
-import com.login.Entity.Role;
 import com.login.Entity.User;
 import com.login.exceptions.UserException;
 
 public interface UserServices {
 	
 	User createUser(User user) throws UserException;
+
 	List<User> getAllUsers()throws UserException;
+
 	User getUserByEmail(String email) throws UserException;
+
 	User getUserById(Long id) throws UserException;
+
 	User deleteUserById(Long id) throws UserException;
+
 	User updateUser(User user) throws UserException;
+
 	
 }
