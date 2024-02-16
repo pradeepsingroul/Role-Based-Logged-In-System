9b5d8e2c4d366107d36cd0414afc452989d21280
predeep kumar singroul <prasingroul4899@gmail.com>
2024-02-16 07:08:59 +0530

    Role repo update

--- Role-Based-Login-System/src/main/java/com/login/repository/RoleRepository.java
+++ Role-Based-Login-System/src/main/java/com/login/repository/RoleRepository.java
@@ -1,11 +1,17 @@
 package com.login.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.login.Entity.Role;
+import java.util.List;
+import com.login.Entity.RoleType;
+
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long>{
 
+	Optional<Role> findByName(RoleType name);
 }
